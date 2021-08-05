e6f5f61a60867893b290adb2f8f62376b9f8b0ed
Juan Hurtado <juan@kommit.co>
2021-08-04 20:39:46 -0500

    Add new string's test cases

--- test/errors/error_string_test.exs
+++ test/errors/error_string_test.exs
@@ -4,13 +4,19 @@ defmodule Error.StringTest do
   alias XDR.Error.String
 
   test "When receives :not_bitstring" do
-    assert_raise String, "the value which you try to encode must be a bitstring value", fn ->
+    assert_raise String, "The value you are trying to encode must be a bitstring value", fn ->
       raise String, :not_bitstring
     end
   end
 
+  test "When receives :invalid_length" do
+    assert_raise String, "The length of the string exceeds the max length allowed", fn ->
+      raise String, :invalid_length
+    end
+  end
+
   test "When receives :not_binary" do
-    assert_raise String, "the value which you try to decode must be a binary value", fn ->
+    assert_raise String, "The value you are trying to decode must be a binary value", fn ->
       raise String, :not_binary
     end
   end
